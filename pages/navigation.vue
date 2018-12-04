cc0b653eaaf427cc8fd167616aaa0afbb9b2ca80
Ivan Codesido <ivan.codesido@gmail.com>
2018-12-04 16:29:44 +0000

    Rogue page was throwing error on generate

--- pages/navigation.vue
+++
@@ -1,7 +0,0 @@
-	<nav>
-	  <div class="title">
-	    <nuxt-link exact to="/">Home page</nuxt-link>
-	    <nuxt-link to="/product">Product page</nuxt-link>
-	    <nuxt-link to="/contact">Contact page</nuxt-link>
-	  </div>
-	</nav>
